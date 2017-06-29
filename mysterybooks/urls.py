400ebde6a998f84a4eb7628dda270e657689de73
Bogachan Arslan <bogacbugra@gmail.com>
2017-06-30 01:28:51 +0300

    Add modules views templates and urls for shopping cart!

--- mysterybooks/urls.py
+++ mysterybooks/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # url(r'^$', 'mysterybooks.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^store/',include('store.urls'),name="store"),
-    url(r'^account/', include('registration.backends.default.urls')),
+    url(r'^store/', include('store.urls'), name="store"),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url('', include('social.apps.django_app.urls', namespace='social')),
 ]
