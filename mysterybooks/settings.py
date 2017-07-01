b8ef3960a4f47f66259b81244873c59d396c81ab
Bogachan Arslan <bogacbugra@gmail.com>
2017-07-01 09:25:16 +0300

    Add file upload for book cover

--- mysterybooks/settings.py
+++ mysterybooks/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'social.apps.django_app.context_processors.backends',
                 'social.apps.django_app.context_processors.login_redirect',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -121,6 +122,9 @@ STATICFILES_FINDERS=(
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL="/media/"
 
 
 #Registration
--- store/models.py
+++ store/models.py
@@ -7,6 +7,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+def cover_upload_path(instance, filename):
+    return '/'.join(['books', str(instance.id), filename])
+
+
 class Author(models.Model):
     first_name=models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -22,6 +26,7 @@ class Book(models.Model):
     publish_date=models.DateField(default=timezone.now)
     price=models.DecimalField(decimal_places=2,max_digits=8)
     stock=models.IntegerField(default=0)
+    cover_image=models.ImageField(upload_to=cover_upload_path, default='books/empty_cover.jpg')
 
 
 class Review(models.Model):
